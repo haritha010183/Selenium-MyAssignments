f9743d059ec26d2fb6356329564f4383456368a3
haritha010183 <48109162+haritha010183@users.noreply.github.com>
2025-06-22 11:43:40 +0530

    Add files via upload
    Week1Day1Assignments

---
+++ Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("Version: " + version + "\nDeveloper: " + developer + "\nIsBeta: " +
+		isBeta + "\nReleaseYear: " + releaseYear + "\nShortCutKey: " + shortcutKey);
+
+	}
+
+}
---
+++ FireFox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		
+		float browserVersion = 100.2f;
+		String browserName = "Firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println("BrowserVersion: " + browserVersion + "\nBrowserName: " + browserName +
+				"\nIsVisible: " + isVisible +"\nRelease Year: " + releaseYear + "\nBrowserLogo: " + 
+				browserLogo);
+
+	}
+
+}

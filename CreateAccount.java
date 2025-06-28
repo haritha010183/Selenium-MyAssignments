eb9a3d7986e0f8421112b7f54f030e6262b29f7c
haritha010183 <48109162+haritha010183@users.noreply.github.com>
2025-06-28 23:44:24 +0530

    Add files via upload
    week2.day1.assignments

--- CreateAccount.java
+++ CreateAccount.java
@@ -11,24 +11,34 @@ public class CreateAccount {
 
 	public static void main(String[] args) {
 		
-		
+		//Creating a new object
 		ChromeDriver driver = new ChromeDriver();
 		
+		//Opening an URL and maximizing the browser window
 		driver.get("http://leaftaps.com/opentaps/");
 		driver.manage().window().maximize();
+		
+		//Entering username and password and Logging in
 		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
 		driver.findElement(By.id("password")).sendKeys("crmsfa");
 		driver.findElement(By.className("decorativeSubmit")).click();
+		
+		//Clicking on a Link and navigating to create an account
 		driver.findElement(By.partialLinkText("CRM")).click();
 		driver.findElement(By.linkText("Accounts")).click();
-		driver.findElement(By.className("x-btn-text")).click();
 		driver.findElement(By.className("selected")).sendKeys("Test Account");
-		driver.findElement(By.name("description")).sendKeys("Selenium Automation Tester");
+		
+		//Entering mandatory information to create a Lead
+		driver.findElement(By.id("accountName")).sendKeys("Selenium Automation Tester");
 		driver.findElement(By.id("numberEmployees")).sendKeys("50");
 		driver.findElement(By.id("officeSiteName")).sendKeys("LeafTaps");
 		driver.findElement(By.className("smallSubmit")).click();
+		
+		//Getting the title of the URL
 		String title = driver.getTitle();
 		System.out.println(title);
+		
+		//Closing the browser
 		driver.close();
 
 	}
--- CreateLead.java
+++ CreateLead.java
@@ -7,24 +7,35 @@ public class CreateLead {
 
 	public static void main(String[] args) {
 		
+		//Creating a new object
 		ChromeDriver driver = new ChromeDriver();
 		
+		//Opening an URL and maximizing the browser window
 		driver.get("http://leaftaps.com/opentaps/");
 		driver.manage().window().maximize();
 		
+		//Entering username and password and Logging in
 		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
 		driver.findElement(By.id("password")).sendKeys("crmsfa");
 		driver.findElement(By.className("decorativeSubmit")).click();
+		
+		//Clicking on a Link and navigating to create a lead
 		driver.findElement(By.partialLinkText("CRM")).click();
 		driver.findElement(By.linkText("Leads")).click();
 		driver.findElement(By.linkText("Create Lead")).click();
+		
+		//Entering mandatory information to create a Lead
 		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Test");
 		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Lead");
 		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
 		driver.findElement(By.id("createLeadForm_generalProfTitle")).sendKeys("Automation Tester");
 		driver.findElement(By.name("submitButton")).click();
+		
+		//Getting the title of the URL
 		String title = driver.getTitle();
 		System.out.println(title);
+		
+		//Closing the browser
 		driver.close();
 
 	}

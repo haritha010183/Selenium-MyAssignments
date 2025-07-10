27d3268870752727080246ce0122cc4d7add5877
haritha010183 <48109162+haritha010183@users.noreply.github.com>
2025-07-10 21:35:25 +0530

    Add files via upload
    Week3.Day2.Assignments

---
+++ Elements.java
@@ -0,0 +1,14 @@
+package week3.day2;
+
+public class Elements extends Button {
+
+	public static void main(String[] args) {
+		Elements method = new Elements();
+		method.click();
+		method.setText();
+		method.submit();
+
+
+	}
+
+}

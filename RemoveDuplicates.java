8d456e481db1200cf4b989dd079cd45eb25bb53c
haritha010183 <48109162+haritha010183@users.noreply.github.com>
2025-07-10 19:52:41 +0530

    Add files via upload
    week3.day1.assignments_removeduplicates

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -5,30 +5,19 @@ public class RemoveDuplicates {
 	public static void main(String[] args) {
 		
 		String text = "We learn Java basics as part of java sessions in java week1";
-		
-		int count = 0;
 	
 		String output[] = text.split(" ");
 		for (int i = 0; i < output.length; i++) {
 			for (int j = i + 1; j < output.length; j++) {
 				if (output[i].equalsIgnoreCase(output[j])) {
-				output[j] =  "";
-				count++;
+				output[j] =  " ";		
 				}
 			}
 		}
 		
-		if (count > 0) {
-			for (String word : output) {
-				if (!word.equals("")) {
-					System.out.print(word + " ");
-				}
+		for (int i = 0; i < output.length; i++) {
+					System.out.print(output[i] + " ");
 			}
-		}
-		
-		else {
-			System.out.println("No duplicate words found");
-		}
 	}
 
 }
